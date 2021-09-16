bdd48cc5e65a8c0d507bcf6031c8af642843fb24
Lucas Ollarce <ollyxs@pop-os.localdomain>
2021-09-16 18:56:14 -0300

    macro bolson

--- frontend/main/routes/bolsones.py
+++ frontend/main/routes/bolsones.py
@@ -1,11 +1,24 @@
 from flask import Blueprint, request, url_for, render_template
 
+
 bolsones = Blueprint('bolsones', __name__, url_prefix='/bolson')
+
+BOLSONES = [
+        {"id":0,"nombre":"Carnes","precio":"$99.99","aprobado":True,"fecha":"2021-09-13","productos":"milanesa"},
+        {"id":1,"nombre":"Panadería","precio":"$99.99","aprobado":True,"fecha":"2021-09-13","productos":"pan"},
+        {"id":2,"nombre":"Verdulería","precio":"$99.99","aprobado":False,"fecha":"2021-09-13","productos":"papa"},
+        {"id":3,"nombre":"Lacteos","precio":"$99.99","aprobado":True,"fecha":"2021-09-13","productos":"leche"},
+        {"id":4,"nombre":"Limpieza","precio":"$99.99","aprobado":True,"fecha":"2021-09-13","productos":"jabón"},
+        {"id":5,"nombre":"Fiambrería","precio":"$99.99","aprobado":True,"fecha":"2021-09-13","productos":"jamon"},
+        {"id":6,"nombre":"Bebidas","precio":"$99.99","aprobado":True,"fecha":"2021-09-13","productos":"vino"},
+        {"id":7,"nombre":"Legumbres","precio":"99.99","aprobado":True,"fecha":"2021-09-13","productos":"fideos"}
+        ]
+
 
 @bolsones.route('/ver/<int:id>')
 def ver(id):
-    return render_template('bolson.html')
+    return render_template('bolson.html', bolson = BOLSONES[id])
 
 @bolsones.route('/ver-todos')
 def ver_todos():
-    return render_template('bolsones.html')
+    return render_template('bolsones.html', bolsones = BOLSONES)
--- frontend/main/routes/main.py
+++ frontend/main/routes/main.py
@@ -1,7 +1,19 @@
 from flask import Blueprint, url_for, render_template
+from . import bolsones
 
 main = Blueprint('main', __name__, url_prefix='/')
 
+BOLSONES = [
+        {"id":0,"nombre":"Carnes","precio":"$99.99","aprobado":True,"fecha":"2021-09-13","productos":"milanesa"},
+        {"id":1,"nombre":"Panadería","precio":"$99.99","aprobado":True,"fecha":"2021-09-13","productos":"pan"},
+        {"id":2,"nombre":"Verdulería","precio":"$99.99","aprobado":False,"fecha":"2021-09-13","productos":"papa"},
+        {"id":3,"nombre":"Lacteos","precio":"$99.99","aprobado":True,"fecha":"2021-09-13","productos":"leche"},
+        {"id":4,"nombre":"Limpieza","precio":"$99.99","aprobado":True,"fecha":"2021-09-13","productos":"jabón"},
+        {"id":5,"nombre":"Fiambrería","precio":"$99.99","aprobado":True,"fecha":"2021-09-13","productos":"jamon"},
+        {"id":6,"nombre":"Bebidas","precio":"$99.99","aprobado":True,"fecha":"2021-09-13","productos":"vino"},
+        {"id":7,"nombre":"Legumbres","precio":"$99.99","aprobado":True,"fecha":"2021-09-13","productos":"fideos"}
+        ]
+
 @main.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', bolsones = BOLSONES)
