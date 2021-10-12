131b0464f225b00101c4b0a3172d9d386b07664d
Lucas Ollarce <ollyxs@pop-os.localdomain>
2021-10-12 15:58:52 -0300

    crear bolson

--- frontend/main/routes/bolsones.py
+++ frontend/main/routes/bolsones.py
@@ -1,43 +1,23 @@
 from flask import Blueprint, redirect, url_for, render_template, current_app
+from .. formularios.bolson import FormBolson
 import requests, json
+
 
 bolsones = Blueprint('bolsones', __name__, url_prefix='/bolson')
 
-@bolsones.route('/ver/<int:id>')
+
+@bolsones.route('/<int:id>')
 def ver(id):
     r = requests.get(
-            current_app.config["API_URL"]+'/bolson-venta/'+str(id),
+            current_app.config["API_URL"]+'/bolson/'+str(id),
             headers = {"content-type": "application/json"})
     if (r.status_code == 404):
         return redirect(url_for('bolsones.ver_todos'))
     bolson = json.loads(r.text)
     return render_template('bolson.html', bolson = bolson)
 
-@bolsones.route('/ver-todos')
-def ver_todos():
-    data = {}
-    data['page'] = 1
-    data['per_page'] = 10
-    print(data)
-    r = requests.get(
-        current_app.config["API_URL"]+'/bolsones-venta',
-        headers = {"content-type":"application/json"},
-        data = json.dumps(data))
-    bolsones = json.loads(r.text)["bolsonesventas"] 
-    return render_template('bolsones.html', bolsones = bolsones)
-
-@bolsones.route('/<int:id>')
-def ver_bolson(id):
-    r = requests.get(
-            current_app.config["API_URL"]+'/bolson/'+str(id),
-            headers = {"content-type": "application/json"})
-    if (r.status_code == 404):
-        return redirect(url_for('bolsones.listar_todos'))
-    bolson = json.loads(r.text)
-    return render_template('bolson.html', bolson = bolson)
-
 @bolsones.route('/todos')
-def listar_todos():
+def ver_todos():
     data = {}
     data['page'] = 1
     data['per_page'] = 10
@@ -49,7 +29,30 @@ def listar_todos():
     bolsones = json.loads(r.text)["bolsones"] 
     return render_template('bolsones_admin.html', bolsones = bolsones)
 
-@bolsones.route('/ver-bolson-pendiente/<int:id>')
+@bolsones.route('/ver/<int:id>')
+def ver_en_venta(id):
+    r = requests.get(
+            current_app.config["API_URL"]+'/bolson-venta/'+str(id),
+            headers = {"content-type": "application/json"})
+    if (r.status_code == 404):
+        return redirect(url_for('bolsones.ver_todos_en_venta'))
+    bolson = json.loads(r.text)
+    return render_template('bolson.html', bolson = bolson)
+
+@bolsones.route('/ver-todos')
+def ver_todos_en_venta():
+    data = {}
+    data['page'] = 1
+    data['per_page'] = 10
+    print(data)
+    r = requests.get(
+        current_app.config["API_URL"]+'/bolsones-venta',
+        headers = {"content-type":"application/json"},
+        data = json.dumps(data))
+    bolsones = json.loads(r.text)["bolsonesventas"] 
+    return render_template('bolsones.html', bolsones = bolsones)
+
+@bolsones.route('/pendiente/<int:id>')
 def ver_pendiente(id):
     r = requests.get(
             current_app.config["API_URL"]+'/bolson-pendiente/'+str(id),
@@ -59,7 +62,7 @@ def ver_pendiente(id):
     bolson = json.loads(r.text)
     return render_template('bolson.html', bolson = bolson)
 
-@bolsones.route('/bolsones-pendientes')
+@bolsones.route('/pendientes')
 def ver_pendientes():
     data = {}
     data['page'] = 1
@@ -71,3 +74,60 @@ def ver_pendientes():
             data = json.dumps(data))
     bolsones = json.loads(r.text)["bolsones pendientes"]
     return render_template('bolsones_admin.html', bolsones = bolsones)
+
+@bolsones.route('/previo/<int:id>')
+def ver_previo(id):
+    r = requests.get(
+            current_app.config["API_URL"]+'/bolson-previo/'+str(id),
+            headers = {"content-type": "application/json"})
+    if (r.status_code == 404):
+        return redirect(url_for('bolsones.ver_todos'))
+    bolson = json.loads(r.text)
+    return render_template('bolson.html', bolson = bolson)
+
+@bolsones.route('/previos')
+def ver_previos():
+    data = {}
+    data['page'] = 1
+    data['per_page'] = 10
+    print(data)
+    r = requests.get(
+        current_app.config["API_URL"]+'/bolsones-previos',
+        headers = {"content-type":"application/json"},
+        data = json.dumps(data))
+    bolsones = json.loads(r.text)["bolsones"] 
+    return render_template('bolsones_admin.html', bolsones = bolsones)
+
+@bolsones.route('/crear', methods=['POST', 'GET'])
+def crear():
+    form = FormBolson()
+    data = {}
+    data['page'] = 1
+    r = requests.get(
+            current_app.config["API_URL"]+'/productos',
+            headers = {"content-type": "application/json"},
+            data = json.dumps(data))
+    productos = [(item['id'], item['nombre']+"\n - "+item['proveedor']) for item in json.loads(r.text)["productos"]]
+    print(productos)
+
+    #productos.insert(0, (0, ""))
+    form.productosId.choices = productos
+    if form.validate_on_submit():
+        headers = {'content-type': 'application/json'}
+        data = {}
+        data["nombre"] = form.nombre.data
+        data["fecha"] = form.fecha.data.strftime("%Y-%m-%d")
+        # productosId = []
+        # if form.productosId.data != 0:
+        #     productosId.append(form.productosId.data)
+        data["productos"] = form.productosId.data
+        print(data)
+        r = requests.post(
+                current_app.config["API_URL"]+'/bolsones-pendientes',
+                headers = headers,
+                data = json.dumps(data))
+        if (r.status_code == 201):
+            return redirect(url_for('bolsones.ver_todos'))
+        print(r)
+    return render_template('crear_bolson.html', form = form)
+        
